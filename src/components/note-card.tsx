4b8909876812d723e79632a82a7b1836849541c8
Gabriel Fuli <gabriel.fuli@capgemini.com>
2024-02-08 17:08:59 -0300

    Last Updates NLW Expert Project: Finished

--- src/components/note-card.tsx
+++ src/components/note-card.tsx
@@ -5,12 +5,14 @@ import {X} from 'lucide-react'
 
 interface NoteCardProps {
     note: {
-        date: Date,
+        id: string
+        date: Date
         content: string
     }
+    onNoteDeleted: (id: string) => void
 }
 
-export function NoteCard({note}: NoteCardProps) {
+export function NoteCard({note, onNoteDeleted}: NoteCardProps) {
 
     return (
         <Dialog.Root>
@@ -27,7 +29,7 @@ export function NoteCard({note}: NoteCardProps) {
 
             <Dialog.Portal>
                 <Dialog.Overlay className='inset-0 fixed bg-black/50' />
-                <Dialog.Content className='fixed overflow-hidden left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 max-w-[640px] w-full h-[60hv] bg-slate-700 rounded-md outline-none flex flex-col'>
+                <Dialog.Content className='fixed inset-0 md:inset-auto overflow-hidden md:left-1/2 md:top-1/2 md:-translate-x-1/2 md:-translate-y-1/2 md:max-w-[640px] w-full md:h-[60hv] bg-slate-700 md:rounded-md outline-none flex flex-col'>
                     <Dialog.Close className='rounded-xl m-1 absolute right-0 bg-slate-800 p-1.5 text-slate-400'>
                         <X className='size-5 rounded-md' />
                     </Dialog.Close>
@@ -42,6 +44,7 @@ export function NoteCard({note}: NoteCardProps) {
 
                     <button
                     type='button'
+                    onClick={() => onNoteDeleted(note.id)}
                     className='w-full bg-slate-800 py-4 text-center text-sm text-slate-300 outline-none group'
                     >
                         Deseja <span className='text-red-400 group-hover:underline'>apagar essa nota</span>?
